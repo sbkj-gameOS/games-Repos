3455f52bebfbc24fbc8eda95a96150eea99e3a2e
14768776836 <784700109@qq.com>
2017-09-21 11:49:38 +0800

    更新获取token值

--- src/main/java/com/steven/util/timer/MyTimerTask.java
+++ src/main/java/com/steven/util/timer/MyTimerTask.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.steven.game.dao.WxTokenMapper;
 import com.steven.game.vo.WxToken;
+import com.steven.util.wx.ConfigUtil;
 import com.steven.util.wx.HttpKit;
 
 import java.util.Date;
@@ -28,7 +29,7 @@ public class MyTimerTask extends TimerTask{
         try {
             /** 请求结果 */
             String result = null;
-            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx97f93285af090c3e&secret=1f9470d9289615b41e0a6b487bbe5b85";
+            String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+ ConfigUtil.APPID+"&secret="+ConfigUtil.APP_SECRECT;
             result = HttpKit.get(url);
             JSONObject jsonTexts = (JSONObject) JSON.parse(result);
             WxToken token = new WxToken();
